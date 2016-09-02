86e0e922c8a2e89bfef0b6283d999d4276541707
gpanagiop <giannis.panagiotopoulos@gmail.com>
2016-09-02 18:41:15 +0300

    pretify code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,46 +1,45 @@
 'use strict';
 
 angular.module('bikes', ['ui.router'])
-.config(function($stateProvider, $urlRouterProvider) {
+    .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
-        
-            // route for the home page
+
+        // route for the home page
             .state('app', {
-                url:'/',
-                views: {
-                    'header': {
-                        templateUrl : 'views/header.html',
-                    },
-                    'content': {
-                        templateUrl : 'views/menu.html',
-                        controller  : 'BikeController as vm'
-                    },
-                }
+            url: '/',
+            views: {
+                'header': {
+                    templateUrl: 'views/header.html',
+                },
+                'content': {
+                    templateUrl: 'views/menu.html',
+                    controller: 'BikeController as vm'
+                },
+            }
 
-            })
+        })
 
-            // route for the bikedetail page
-            .state('app.bikedetails', {
-                url: 'menu/:id',
-                views: {
-                    'content@': {
-                        templateUrl : 'views/bikedetail.html',
-                        controller  : 'BikeDetailController as vm'
-                   }
+        // route for the bikedetail page
+        .state('app.bikedetails', {
+            url: 'menu/:id',
+            views: {
+                'content@': {
+                    templateUrl: 'views/bikedetail.html',
+                    controller: 'BikeDetailController as vm'
                 }
-            })
+            }
+        })
 
-            // route for the add bike page
-            .state('app.addbike', {
-                url: 'add',
-                views: {
-                    'content@': {
-                        templateUrl : 'views/add.html',
-                        controller  : 'AddBikeController as vm'
-                   }
+        // route for the add bike page
+        .state('app.addbike', {
+            url: 'add',
+            views: {
+                'content@': {
+                    templateUrl: 'views/add.html',
+                    controller: 'AddBikeController as vm'
                 }
-            });
-    
+            }
+        });
+
         $urlRouterProvider.otherwise('/');
-    })
-;
+    });
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,71 +2,70 @@
 
 angular.module('bikes')
 
-            .controller('BikeController', ['bikeFactory', function(bikeFactory) {
-            var self = this;    
+.controller('BikeController', ['bikeFactory', function(bikeFactory) {
+    var self = this;
 
-            self.bikes= bikeFactory.getBikes();
+    self.bikes = bikeFactory.getBikes();
 
-        }])
+}])
 
-        .controller('BikeDetailController', ['$stateParams', 'bikeFactory', '$location', function($stateParams, bikeFactory, $location) {
+.controller('BikeDetailController', ['$stateParams', 'bikeFactory', '$location', function($stateParams, bikeFactory, $location) {
 
-            var self = this;
-            var bike= bikeFactory.getBike(parseInt($stateParams.id,10));
-            
-            self.bike = bike;
-            self.bikes= bikeFactory.getBikes();
+    var self = this;
+    var bike = bikeFactory.getBike(parseInt($stateParams.id, 10));
 
-            // update bike
-            self.updateBike = function (item) {
-                bike = {
-               _id: bike._id,
-               model: bike.model,
-               brand: bike.brand,
-               category: bike.category,
-               year: bike.year
-                };
-                location.href = "/bike_garage/app/#";
-            }
+    self.bike = bike;
+    self.bikes = bikeFactory.getBikes();
 
-            //delete bike
-              self.remove = function(item) {
-                _.remove(self.bikes, function(bike) {
-                  return item._id === bike._id;
-                  
-                });
-                 location.href = "/bike_garage/app/#";
-              };
+    // update bike
+    self.updateBike = function(item) {
+        bike = {
+            _id: bike._id,
+            model: bike.model,
+            brand: bike.brand,
+            category: bike.category,
+            year: bike.year
+        };
+        location.href = "/bike_garage/app/#";
+    }
 
-        }])
+    //delete bike
+    self.remove = function(item) {
+        _.remove(self.bikes, function(bike) {
+            return item._id === bike._id;
 
-        
-        .controller('AddBikeController', ['bikeFactory', '$location', function(bikeFactory, $location) {
+        });
+        location.href = "/bike_garage/app/#";
+    };
 
-            var self = this;
-            self.bike = [];
-            self.bikes= bikeFactory.getBikes();
+}])
 
-            //Find the maximun id 
-            self.maxId = function() {
-                var maxid =0;
-                self.bikes.map(function(obj){     
-                    if (obj._id > maxid) maxid = obj._id;    
-                });
-                return maxid;
-            }
 
-            self.mybike = {_id:self.maxId(self.bikes) + 1, model:"", brand:"", image:"images/default.jpg", category:"", year:""};
-            
-            self.addBike = function () {
-                
-                console.log(self.mybike);
-                
-                self.bikes.push(self.mybike);
-                location.href = "/bike_garage/app/#";
-                
-                self.mybike = {_id:self.maxId(self.bikes)+1, model:"", brand:"", image:"", category:"", year:""};
-            }
+.controller('AddBikeController', ['bikeFactory', '$location', function(bikeFactory, $location) {
 
-        }])
-;
+    var self = this;
+    self.bike = [];
+    self.bikes = bikeFactory.getBikes();
+
+    //Find the maximun id 
+    self.maxId = function() {
+        var maxid = 0;
+        self.bikes.map(function(obj) {
+            if (obj._id > maxid) maxid = obj._id;
+        });
+        return maxid;
+    }
+
+    self.mybike = { _id: self.maxId(self.bikes) + 1, model: "", brand: "", image: "images/default.jpg", category: "", year: "" };
+
+    self.addBike = function() {
+
+        console.log(self.mybike);
+
+        self.bikes.push(self.mybike);
+        location.href = "/bike_garage/app/#";
+
+        self.mybike = { _id: self.maxId(self.bikes) + 1, model: "", brand: "", image: "", category: "", year: "" };
+    }
+
+}]);
